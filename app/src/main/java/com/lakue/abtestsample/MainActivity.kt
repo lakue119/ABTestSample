c1734840e19102ccae9d9dd9b208e546bd1a2f6d
ChoiMyungRak <audfkr0808@naver.com>
2021-12-25 04:32:15 +0900

    A/B Test 연동

--- app/src/main/java/com/lakue/abtestsample/MainActivity.kt
+++ app/src/main/java/com/lakue/abtestsample/MainActivity.kt
@@ -2,10 +2,37 @@ package com.lakue.abtestsample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.google.firebase.installations.FirebaseInstallations
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.FirebaseRemoteConfigSettings
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
 
 class MainActivity : AppCompatActivity() {
+
+    val TAG = "Installations"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val remoteConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        remoteConfig.setDefaultsAsync(R.xml.remote_config_defaults)
+
+        remoteConfig.fetchAndActivate()
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    val data = remoteConfig.getString("button_style")
+                    Log.d(TAG, "Config params updated: $data")
+                } else {
+                    Log.d(TAG, "Fail")
+                }
+            }
     }
 }
